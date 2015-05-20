25a03d2bdbf1e92af49042b9960ea9138fc99317
Matt Keef <mkeef1@gmail.com>
2015-05-20 01:27:36 -0500

    bullets need fixin

--- Resources/MainScene.js
+++ Resources/MainScene.js
@@ -7,11 +7,14 @@
 		var left = null,
 			player = null,
 			right = null;
+			fire = null;
+			MAX_BULLETS = 10;
 			
 		var transform = platino.createTransform();
 		
+		var bullets = new Array(MAX_BULLETS);
+		
 		transform.duration = 2000;
-		transform.x = 0;
 		
 		// Report touch events here
 		var onSpriteTouch = function(e) {
@@ -20,6 +23,7 @@
 		
 		var onLeftTouch = function(e) {
 			if(e.type == 'touchstart') {
+				transform.x = 0;
 				player.transform(transform);
 			} else {
 				player.clearTransforms();
@@ -32,7 +36,14 @@
 				player.transform(transform);
 			} else {
 				player.clearTransforms();
-				transform.x = 0;
+			}
+		};
+		
+		var onFireTouch = function(e) {
+			if(e.type == 'touchstart') {
+				transform.y = 0;
+				bullets[0].transform(transform);
+				bullets[0].show();
 			}
 		};
 		
@@ -41,44 +52,50 @@
 
 			Ti.API.info("MainScene has been activated.");
 			
+			
+			
+			fire = platino.createSprite({
+				width: game.TARGET_SCREEN.width - 100, height: 50, x: game.TARGET_SCREEN.width / 4, y: game.TARGET_SCREEN.height - 128
+			});
+			
 			left = platino.createSprite({
-				width: 50,
-				height: 50,
-				x: 10,
-				y: game.TARGET_SCREEN.height - 128
+				width: 50, height: 50, x: 10, y: game.TARGET_SCREEN.height - 128
 			});
 			left.color(0, 0, 1.0);
 			left.name = 'Left';
 			
 			player = platino.createSprite({
-				width: 50,
-				height: 50,
-				x: 100,
-				y: game.TARGET_SCREEN.height - 64
+				width: 50, height: 50, x: game.TARGET_SCREEN.width / 2, y: game.TARGET_SCREEN.height - 64
 			});
 			player.color(1.0, 0, 0);
 			player.name = 'player';
 
 			right = platino.createSprite({
-				width: 50,
-				height: 50,
-				x: 320,
-				y: game.TARGET_SCREEN.height - 128
+				width: 50, height: 50, x: game.TARGET_SCREEN.width, y: game.TARGET_SCREEN.height - 128
 			});
-			right.color(0, 1.0, 0);
+			right.color(0, 0, 1.0);
 			right.name = 'right';
+			
+			function makeBullets() {
+				for (var i; i < MAX_BULLETS; i++) {
+					bullets[i] = platino.createSprite({width: 10, height: 10, x: (player.x / 2) - 5, y: (player.y / 2) - 5});
+					bullets[i].hide();
+					scene.add(bullets[i]);
+				}
+			}
 			
 			scene.add(left);
 			scene.add(player);
 			scene.add(right);
+			scene.add(fire);
 			
 			// add touch events to sprites
 			left.addEventListener('touchstart', onLeftTouch);
 			left.addEventListener('touchend', onLeftTouch);
 			right.addEventListener('touchstart', onRightTouch);
 			right.addEventListener('touchend', onRightTouch);
-			player.addEventListener('touchstart', onSpriteTouch);
-			player.addEventListener('touchend', onSpriteTouch);
+			fire.addEventListener('touchstart', onFireTouch);
+			fire.addEventListener('touchend', onFireTouch);
 		};
 
 		var onSceneDeactivated = function(e) {
@@ -89,8 +106,8 @@
 			
 			if (left) {
 				scene.remove(left);
-				left.removeEventListener('touchstart', onSpriteTouch);
-				left.removeEventListener('touchend', onSpriteTouch);
+				left.removeEventListener('touchstart', onLeftTouch);
+				left.removeEventListener('touchend', onLeftTouch);
 				left = null;
 			}
 
@@ -103,9 +120,16 @@
 
 			if (right) {
 				scene.remove(right);
-				right.removeEventListener('touchstart', onSpriteTouch);
-				right.removeEventListener('touchend', onSpriteTouch);
+				right.removeEventListener('touchstart', onRightTouch);
+				right.removeEventListener('touchend', onRightTouch);
 				right = null;
+			}
+
+			if (fire) {
+				scene.remove(fire);
+				fire.removeEventListener('touchstart', onSpriteTouch);
+				fire.removeEventListener('touchend', onSpriteTouch);
+				fire = null;
 			}
 
 			touchable = null;
